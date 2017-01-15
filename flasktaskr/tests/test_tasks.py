c836939846bbe35cb9e68ae07a7b143b99a79393
Brian Hoppus <brian@hoppus.io>
2017-01-15 14:35:00 -0800

    Use bcrypt to encrypt all user passwords

--- flasktaskr/tests/test_tasks.py
+++ flasktaskr/tests/test_tasks.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from project import app, db
+from project import app, db, bcrypt
 from project._config import basedir
 from project.models import Task, User
 
@@ -49,7 +49,11 @@ class TasksTests(unittest.TestCase):
         return self.app.get("logout/", follow_redirects=True)
 
     def create_user(self, name, email, password):
-        new_user = User(name=name, email=email, password=password)
+        new_user = User(
+            name=name,
+            email=email,
+            password=bcrypt.generate_password_hash(password)
+        )
         db.session.add(new_user)
         db.session.commit()
 
@@ -66,7 +70,7 @@ class TasksTests(unittest.TestCase):
         new_user = User(
             name="Superman",
             email="admin@realpython",
-            password="allpowerfull",
+            password=bcrypt.generate_password_hash("allpowerful"),
             role="admin"
         )
 
@@ -201,7 +205,7 @@ class TasksTests(unittest.TestCase):
         new_user = User(
             name="Superman",
             email="admin@realpython.com",
-            password="allpowerful",
+            password=bcrypt.generate_password_hash("allpowerful"),
             role="admin"
         )
         db.session.add(new_user)
